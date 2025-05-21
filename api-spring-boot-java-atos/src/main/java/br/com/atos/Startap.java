6a0ef3ba96f35531558efe7abdbfe3128c31f61f
AvohaiTeixeira <avohai.atos@gmail.com>
2025-05-20 23:28:18 -0300

    Primeiro commit com a estruturação do projeto

---
+++ api-spring-boot-java-atos/src/main/java/br/com/atos/Startap.java
@@ -0,0 +1,14 @@
+package br.com.atos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startap {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Startap.class, args);
+	}
+
+}
---
+++ api-spring-boot-java-atos/src/main/java/br/com/atos/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.atos.model;
+
+public record Greeting(Long id, String content) {
+}
